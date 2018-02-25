52c91d53ebb704cb650dbda2e1bee71ce9fb5815
Samuel Knepper <sambobkne@gmail.com>
2018-02-25 12:48:40 -0600

    build SearchResults Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import SearchBar from './components/SearchBar/SearchBar';
 import './App.css';
+import SearchResults from './components/SearchResults/SearchResults';
 
 function App() {
   return (
-    <SearchBar />
+    <div className="App">
+      <SearchBar />
+      <div className="App-playlist">
+        <SearchResults />
+      </div>
+    </div>
   );
 }
 
